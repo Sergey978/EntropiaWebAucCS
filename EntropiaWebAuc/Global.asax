758789fb2ca829452a74040ca8ab0782d358e1cb
Sergey <vasilevsky.s@gmail.com>
2017-07-25 07:57:55 +0300

    First commit

---
+++ EntropiaWebAuc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntropiaWebAuc.MvcApplication" Language="C#" %>
---
+++ EntropiaWebAuc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EntropiaWebAuc.Startup))]
+namespace EntropiaWebAuc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
