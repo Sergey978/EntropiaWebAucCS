1fc7a1655e286bdb37a1bda1796edb8ae253dc0b
Sergey <vasilevsky.s@gmail.com>
2017-07-27 18:07:58 +0200

    Areas added

---
+++ EntropiaWebAuc/Domain/Abstract/IStandartItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntropiaWebAuc.Entities;
+
+namespace EntropiaWebAuc.Abstract
+{
+    public interface IStandartItemRepository
+    {
+        IQueryable<StandartItem> StandartItems { get; }
+    }
+}
