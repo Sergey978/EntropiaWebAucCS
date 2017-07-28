7dc30735757fa76054a531e2a41df23053c77601
Sergey <vasilevsky.s@gmail.com>
2017-07-28 18:02:31 +0200

    admin area tuning

--- EntropiaWebAuc/App_Start/BundleConfig.cs
+++ EntropiaWebAuc/App_Start/BundleConfig.cs
@@ -26,6 +26,11 @@ namespace EntropiaWebAuc
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Admin/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/Admin/styles.css"
+                ));
         }
     }
 }
