d47b18c247ea99f81340ae9e24ec36d54b34a420
Sergey <vasilevsky.s@gmail.com>
2018-01-06 11:10:31 +0200

    new template in proces

---
+++ EntropiaWebAuc/Helpers/HtmlHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntropiaWebAuc.Helpers
+{
+    public static class HtmlHelpers
+    {
+        // Example using :<li class="@Html.IsActive("Default", "Index")"><a href="@Url.Action("Index","Default")">
+        public static string IsActive(this HtmlHelper htmlHelper, string controller, string action)
+        {
+            var routeData = htmlHelper.ViewContext.RouteData;
+
+            var routeAction = routeData.Values["action"].ToString();
+            var routeController = routeData.Values["controller"].ToString();
+
+            var returnActive = (controller == routeController && action == routeAction);
+
+            return returnActive ? "active" : "";
+        }
+    }
+}
