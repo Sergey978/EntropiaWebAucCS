c57367f5ec1ffd47c622b91c13564a08ca6421b4
sergey <vasilevsky.s@gmail.com>
2018-01-15 19:16:06 +0200

    mesagges in progress

---
+++ EntropiaWebAuc/Areas/Default/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntropiaWebAuc.Areas.Default.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Default/Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+     
+
+        // POST: Default/Contact/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ EntropiaWebAuc/Areas/Default/ViewModels/ContactViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntropiaWebAuc.Areas.Default.ViewModels
+{
+    public class ContactViewModel
+    {
+        int Id { get; set; }
+        String Name { get; set; }
+        String Email { get; set; }
+        String Title { get; set; }
+        String Text { get; set; }
+    }
+}
